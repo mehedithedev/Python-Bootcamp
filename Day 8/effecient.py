f6ed423e748876e7b5232bfc5bc38ff4c0ef0d23
mehedithedev <mehedi.developer@icloud.com>
2024-05-23 15:54:07 -0600

    ending the day

--- Day 8/effecient.py
+++ Day 8/effecient.py
@@ -4,69 +4,76 @@ todos = readFile.readlines()
 while True:
 
     # Get user input and strip space chars from it
-    userChoice = input("Choose any of this: a, s, e, d, x: \n")
-    match userChoice:
-
-        case "a":
-            addTodo = input("Enter a ToDo item: ") + "\n"
-
-            with open('list.txt', 'r') as readFile:
-                todoItems = readFile.readlines()
-            todoItems.append(addTodo)
-
-            writeFile = open("list.txt", "w")
-            writeFile.writelines(todoItems)
-            writeFile.close()
+    userChoice = input("Choose any of this: add, show, edit, complete, exit: \n")
 
 
-            with open('list.txt', 'w') as readFile:
-                readFile.writelines(todoItems)
-        case "e":
-            todoEdit = int(input("Enter the index of todo you want to edit: "))
-            todoEdit = todoEdit -1
+    if "add" in userChoice or "new" in userChoice:
+        addTodo = userChoice[4:]
 
-            with open('list.txt', 'r') as file:
-                todos = file.readlines()
+        with open('list.txt', 'r') as readFile:
+            todoItems = readFile.readlines()
+        todoItems.append(addTodo)
 
-            editedTodo = input("Choose your replace todo item: ")
-            todos[todoEdit] = editedTodo + "\n"
+        writeFile = open("list.txt", "w")
+        writeFile.writelines(todoItems)
+        writeFile.close()
 
-            with open('list.txt', 'w') as file:
-                file.writelines(todos)
 
-        case "d":
-            deleteTask = int(input("Number of todo you want to delete: "))
+        with open('list.txt', 'w') as readFile:
+            readFile.writelines(todoItems)
 
-            with open('list.txt', 'r') as file:
-                todos = file.readlines()
-            index = deleteTask-1
+    elif "show" in userChoice:
+        updatedTodo = open("list.txt", "r")
+        finalTodo = updatedTodo.readlines()
+        updatedTodo.close()
 
-            todo_to_remove = todos[index].strip("\n")
-            todos.pop(index)
+        with open('list.txt', 'r') as readFile:
+            finalTodo = readFile.readlines()
+        # for item in finalTodo:
+        #     new_item = item.strip('\n')
+        #     new_todos.append(new_item)
+        # Below is by using list comparhansion
 
-            with open('list.txt', 'w') as file:
-                file.writelines(todos)
+        # new_todos = [item.strip('\n') for item in finalTodo]
 
-            message = f"Todo {todo_to_remove} was remove from the list"
-            print(message)
-        case "s":
-            updatedTodo = open("list.txt", "r")
-            finalTodo = updatedTodo.readlines()
-            updatedTodo.close()
+        for index, item in enumerate(finalTodo):
+            item = item.strip("\n")
+            print(f"{index + 1} - {item}")
 
-            with open('list.txt', 'r') as readFile:
-                finalTodo = readFile.readlines()
-            # for item in finalTodo:
-            #     new_item = item.strip('\n')
-            #     new_todos.append(new_item)
-            # Below is by using list comparhansion
 
-            # new_todos = [item.strip('\n') for item in finalTodo]
+    elif "edit" in userChoice:
+        todoEdit = int(userChoice[5:])
+        todoEdit = todoEdit -1
 
-            for index, item in enumerate(finalTodo):
-                item = item.strip("\n")
-                print(f"{index + 1} - {item}")
+        with open('list.txt', 'r') as file:
+            todos = file.readlines()
 
-        case "x":
-            print("Bye !")
-            break
+        editedTodo = input("Choose your replace todo item: ")
+        todos[todoEdit] = editedTodo + "\n"
+
+        with open('list.txt', 'w') as file:
+            file.writelines(todos)
+
+    elif "complete" in userChoice:
+        deleteTask = int(userChoice[9:])
+
+        with open('list.txt', 'r') as file:
+            todos = file.readlines()
+        index = deleteTask-1
+
+        todo_to_remove = todos[index].strip("\n")
+        todos.pop(index)
+
+        with open('list.txt', 'w') as file:
+            file.writelines(todos)
+
+        message = f"Todo {todo_to_remove} was remove from the list"
+        print(message)
+
+    elif "exit" in userChoice:
+        print("Closing the program -- ")
+        break
+    else:
+        print("Command is not valid")
+
+print("Bye !")
