29c9208477ce0337f84e8d3ce6f902d776590502
Mehedi Hasan <mehedi.developer@icloud.com>
2024-06-25 20:43:28 +0000

    added description, image and links

--- Intermidiate/Day 22/main.py
+++ Intermidiate/Day 22/main.py
@@ -22,17 +22,23 @@ content2 = """
 
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 # Importing data.csv file
-df = pandas.read_csv("/data.csv", sep=";")
+df = pandas.read_csv("/workspaces/Python-Bootcamp/Intermidiate/Day 22/data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row['title'])
+        st.write(row["description"])
+        image_path = "/workspaces/Python-Bootcamp/Intermidiate/Day 22/images/" + row["image"]
+        st.image(image_path)
+        st.write(f"[Source Code]({row['url']})")    ### This is a special type of string
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
-
+        st.write(row["description"])
+        image_path = "/workspaces/Python-Bootcamp/Intermidiate/Day 22/images/" + row["image"]
+        st.image(image_path)
 
