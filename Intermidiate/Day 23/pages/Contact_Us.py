54663457fc57786e6e8cd37f00ef8543ec79cf4d
Mehedi Hasan <mehedi.developer@icloud.com>
2024-06-26 02:53:23 +0000

    added email feature

--- Intermidiate/Day 23/pages/Contact_Us.py
+++ Intermidiate/Day 23/pages/Contact_Us.py
@@ -1,10 +1,13 @@
 import streamlit as st
+from send_email import send_email
+
 
 st.header("Contact Me")
 
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email address")
     message = st.text_area("Your message")
+    message = message + "\n" + user_email
     button = st.form_submit_button()
     if button:
-        message = message + user_email
+        send_email(message)
--- Intermidiate/Day 23/send_email.py
+++ Intermidiate/Day 23/send_email.py
@@ -1,19 +1,17 @@
 import smtplib, ssl
-host = "smtp.gmail.com"
-port = 465
-username = "mahadihasanbinmahadi@gmail.com"
-password = "bdndecjrnliiydgw"
 
-receiver = "mehedithedev@gmail.com"
 
-context = ssl.create_default_context()
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "mahadihasanbinmahadi@gmail.com"
+    password = "bdndecjrnliiydgw"
 
-message = """\
-Subject: Hi!
-How are you?
-Bye!
+    receiver = "mehedithedev@gmail.com"
 
-"""
-with smtplib.SMTP_SSL(host=host, port=port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host=host, port=port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
