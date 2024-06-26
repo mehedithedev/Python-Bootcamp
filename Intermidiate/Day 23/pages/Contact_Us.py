f31582d36fc3accd2c55e36d2b1aeb19daaf7bcd
Mehedi Hasan <mehedi.developer@icloud.com>
2024-06-26 03:01:01 +0000

    it is up and running

--- Intermidiate/Day 23/pages/Contact_Us.py
+++ Intermidiate/Day 23/pages/Contact_Us.py
@@ -6,8 +6,15 @@ st.header("Contact Me")
 
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email address")
-    message = st.text_area("Your message")
-    message = message + "\n" + user_email
+    raw_message = st.text_area("Your message")
+    message = f"""\
+Subject: New email from {user_email}
+
+From: {user_email}
+{raw_message}
+
+"""
     button = st.form_submit_button()
     if button:
         send_email(message)
+        st.info("Your email was sent successfully")
