23b0f987651d1b2dab92bd80f88518df7deb96b0
Mehedi Hasan <mehedi.developer@icloud.com>
2024-06-30 14:25:23 -0600

    added line on pdf file

---
+++ Intermidiate/Day 24/main.py
@@ -0,0 +1,17 @@
+from fpdf import FPDF
+import pandas as pd
+
+
+pdf = FPDF(orientation="P", unit="mm", format = "A4")
+
+df = pd.read_csv("topics.csv")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times", style='B', size=24)
+    pdf.set_text_color(100,100,100)
+    pdf.cell(w=0, h=24, txt=row['Topic'], align="L", ln=1)
+    pdf.line(x1=10, y1=27, x2=200, y2=27)
+
+
+pdf.output("output.pdf")
