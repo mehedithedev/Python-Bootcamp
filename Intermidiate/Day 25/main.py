a6d2c4f96c0eb354c4f16601d95a8437524dd449
Mehedi Hasan <mehedi.developer@icloud.com>
2024-06-30 18:12:18 -0600

    create a minimal pdf file

---
+++ Intermidiate/Day 25/main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import glob
+from fpdf import FPDF
+from pathlib import Path
+filepaths = glob.glob("Invoices/*.xlsx")
+for filepath in filepaths:
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    filename = Path(filepath).stem
+    invoice_nr = filename.split("-")[0]
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}")
+    pdf.output(f"PDFS/{filename}.pdf")
