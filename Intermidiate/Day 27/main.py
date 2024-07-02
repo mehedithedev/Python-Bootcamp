0c91b0d73da6142a5d42f79a9c916a03bbc557c9
Mehedi Hasan <mehedi.developer@icloud.com>
2024-07-01 22:47:27 -0600

    sent email with news update

--- Intermidiate/Day 27/main.py
+++ Intermidiate/Day 27/main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "872a2184158643219aee2679e6484bdd"
 url = f"https://newsapi.org/v2/everything?q=apple&sortBy=publishedAt&apiKey={api_key}"
@@ -11,7 +12,20 @@ r = requests.get(url)
 # Get a dictionary with data
 content = r.json()
 
+body = ""
 
 # Access the article titles and description
 for article in content["articles"]:
-    print(article["title"])
+    title = article["title"]
+    description = article['description']
+    if title is not None:
+        body = body + title + "\n" + description + 2*"\n"
+
+body = body.encode("utf-8")
+
+send_email(message=body)
+
+
+
+
+
