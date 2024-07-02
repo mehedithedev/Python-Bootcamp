2ba2e0b8e33c3a6732efbcf7ee392b0c0d5e7207
Mehedi Hasan <mehedi.developer@icloud.com>
2024-07-01 23:30:29 -0600

    the program is final

--- Intermidiate/Day 27/main.py
+++ Intermidiate/Day 27/main.py
@@ -1,31 +1,37 @@
 import requests
 from send_email import send_email
 
+topic = "apple"
 api_key = "872a2184158643219aee2679e6484bdd"
-url = f"https://newsapi.org/v2/everything?q=apple&sortBy=publishedAt&apiKey={api_key}"
-
+url = (f"https://newsapi.org/v2/everything?"
+       f"q={topic}"
+       f"&sortBy=publishedAt"
+       f"&language=en"
+       f"&apiKey={api_key}")
 
 # Make request
 r = requests.get(url)
 
-
 # Get a dictionary with data
 content = r.json()
 
-body = ""
+# Start the body with the subject line
+body = "Subject: Today's Top News\n\n"
 
-# Access the article titles and description
-for article in content["articles"]:
-    title = article["title"]
-    description = article['description']
-    if title is not None:
-        body = body + title + "\n" + description + 2*"\n"
+# Check if 'articles' is in the content dictionary
+if 'articles' in content:
+    # Access the article titles and description
+    for article in content["articles"][:20]:
+        title = article.get("title")
+        description = article.get('description')
+        address = article.get("url")
+        if title and description and address:
+            body = body + title + "\n" + description +"\n"+ address + 2*"\n"
 
-body = body.encode("utf-8")
+    # Encode the body variable
+    body = body.encode("utf-8")
 
-send_email(message=body)
-
-
-
-
-
+    # Send the email
+    send_email(body)
+else:
+    print("No articles found in the response.")
