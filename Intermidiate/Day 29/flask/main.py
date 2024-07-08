f877752f55a6044ebc53d168d2b8e3a2d648f183
Mehedi Hasan <mehedi.developer@icloud.com>
2024-07-07 23:27:07 -0600

    added flask

---
+++ Intermidiate/Day 29/flask/main.py
@@ -0,0 +1,17 @@
+import pandas
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    # df = pandas.read_csv("")
+    temperature = 24
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
+
+if __name__ == "__main__":
+    app.run(debug=True)
